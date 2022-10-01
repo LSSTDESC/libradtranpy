4e4114f5bb8111920b6bfad35decc907b8910eb0
dagoret <dagoret@lal.in2p3.fr>
2022-10-01 11:13:00 +0200

    first import of python wrapper for libradtran, imporvements to be done

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # libradtranpy
 libradtran python wrapper
+
+
+Environnement variable LIBRADTRANDIR must be set to libradtran installation path under which one have /bin /data /include /lib and /share of libradtran installation directory. 
+
+ex:
+
+ls $LIBRADTRANDIR
+bin                     data                    include                 lib                     libRadtran-2.0.3        libradtran-2.0.3.tar.gz share 
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+   name='libradtranpy',
+   version='0.1.0',
+   author='Sylvie Dagoret-Campagne',
+   author_email='sylvie.dagoret-campagne@ijclab.in2p3.fr',
+   packages=['libradtranpy', 'test'],
+   scripts=['bin/script.sh'],
+   url='https://github.com/LSSTDESC/libradtranpy',
+   license='LICENSE',
+   description='python wrapper for libradtran',
+   long_description=open('README.md').read(),
+   install_requires=[
+       "Django >= 1.1.1",
+       "pytest",
+   ],
+)
+
