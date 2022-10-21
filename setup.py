1a777d1cbc2a4716e917325bd5b846fb0e9ccb60
Joseph Chevalier <joe_ontheflex@hotmail.fr>
2022-10-21 11:24:19 +0200

    Added tools as a module. Used new functions in tools.equivalentWidth to compute O2 EQW on libradtran simus and plot eqw=f(air mass). This is preliminary work in order to use auxtel's observations of the O2 band to identify the most suitable properties of the atmosphere that describes the observations (link between libradtran and obs.)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
    version='0.1.0',
    author='Sylvie Dagoret-Campagne',
    author_email='sylvie.dagoret-campagne@ijclab.in2p3.fr',
-   packages=['libradtranpy', 'test'],
+   packages=['libradtranpy', 'test', 'tools'],
    scripts=['bin/script.sh'],
    url='https://github.com/LSSTDESC/libradtranpy',
    license='LICENSE',
