876aea76325854ae0d284e1078e757a4f4b14135
Sylvie Dagoret-Campagne <sylvie.dagoret-campagne@ijclab.in2p3.fr>
2023-10-20 21:31:26 +0200

    update

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup
-
 setup(
    name='libradtranpy',
    version='0.2.0',
    author='Sylvie Dagoret-Campagne',
    author_email='sylvie.dagoret-campagne@ijclab.in2p3.fr',
-   packages=['libradtranpy', 'test'],
+   packages=['libradtranpy','tools','test'],
    scripts=['bin/script.sh'],
    url='https://github.com/LSSTDESC/libradtranpy',
    license='LICENSE',
