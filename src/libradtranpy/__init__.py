d9d3594869d006c5d7b6b4bf44d943a96cdbcca4
Sylvie Dagoret-Campagne <sylvie.dagoret-campagne@ijclab.in2p3.fr>
2023-10-23 17:00:07 +0200

    checked the different site simulation is OK

--- src/libradtranpy/__init__.py
+++ src/libradtranpy/__init__.py
@@ -1,3 +1,3 @@
 from .example_module import greetings, meaning
-
-__all__ = ["greetings", "meaning"]
+from .libsimulateVisible import Dict_Of_sitesAltitudes,Dict_Of_sitesPressures
+__all__ = ["greetings", "meaning","Dict_Of_sitesAltitudes","Dict_Of_sitesPressures"]
